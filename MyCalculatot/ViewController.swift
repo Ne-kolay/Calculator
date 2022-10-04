7d1651244554311d1f747b7d93a2663fc4332388
Nikolay Savchenko <Yezzzhun@gmail.com>
2022-10-04 19:06:49 +0300

    update

--- MyCalculatot/ViewController.swift
+++ MyCalculatot/ViewController.swift
@@ -14,9 +14,10 @@ class ViewController: UIViewController {
     var sign: String = ""
     var result = Double()
     
-    var continueTyping = false
+    var firstNumberIsEntered = true
     var secondValueIsEntered: Bool = false
     var signIsselected: Bool = false
+    var numberIsPositive: Bool = true
     
     func calculation (firstElement: Double, secondElement: Double, sign: String) -> Double {
         var result: Double
@@ -48,65 +49,73 @@ class ViewController: UIViewController {
     
     
     @IBAction func digitTapped(_ sender: UIButton) {
-        if continueTyping == false {
-            screen.text = screen.text! + String(sender.tag)
-        } else {
+        if firstNumberIsEntered {
             screen.text = String(sender.tag)
-            continueTyping = false
+            firstNumberIsEntered = false
+        } else {
+            screen.text = screen.text! + String(sender.tag)
+        }
+    }
+    
+    @IBAction func commaTapped(_ sender: UIButton) {
+        guard !firstNumberIsEntered else {return}
+        screen.text = screen.text! + "."
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+        firstValue = 0
+        secondValue = 0
+        sign = ""
+        result = 0
+        firstNumberIsEntered = true
+        secondValueIsEntered = false
+        signIsselected = false
+        numberIsPositive = true
+        screen.text = ""
+    }
+    
+    @IBAction func changeNumberSignTapped(_ sender: UIButton) {
+        guard firstNumberIsEntered == false && screen.text != "0" else {return}
+        guard screen.text != "+" && screen.text != "-" && screen.text != "*" && screen.text != "/" else {return}
+        if numberIsPositive {
+            screen.text = "-" + screen.text!
+            numberIsPositive = false
+        } else {
+            screen.text!.remove(at: screen.text!.startIndex)
+            numberIsPositive = true
         }
     }
     
     
+    
+    
     @IBAction func mathSignTapped(_ sender: UIButton) {
+        guard signIsselected == false else {return}
         guard screen.text?.isEmpty == false else {return}
+        
         firstValue = Double(screen.text!)!
         switch sender.tag {
-        case 11:
+        case 10:
             sign = "+"
             screen.text = "+"
+        case 11:
+            sign = "-"
+            screen.text = "-"
+        case 12:
+            sign = "*"
+            screen.text = "*"
+        case 13:
+            sign = "/"
+            screen.text = "/"
         default:
             return
         }
         signIsselected = true
-        continueTyping = true
+        firstNumberIsEntered = true
+        numberIsPositive = true
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
- 
-    
-    @IBAction func plusTapped(_ sender: UIButton) {
-        guard screen.text?.isEmpty == false else {return}
-        firstValue = Double(screen.text!)!
-        sign = "+"
-        screen.text = "+"
-        signIsselected = true
-        continueTyping = true
-    }
-    @IBAction func minusTapped(_ sender: UIButton) {
-        firstValue = Double(screen.text!)!
-        sign = "-"
-        screen.text = "-"
-        signIsselected = true
-    }
-    @IBAction func multiplyTapped(_ sender: UIButton) {
-        firstValue = Double(screen.text!)!
-        sign = "*"
-        screen.text = "*"
-        signIsselected = true
-    }
-    @IBAction func divideTapped(_ sender: UIButton) {
-        firstValue = Double(screen.text!)!
-        sign = "/"
-        screen.text = "/"
-        signIsselected = true
-    }
+
     
     @IBAction func equalsTapped(_ sender: UIButton) {
         guard signIsselected == true && screen.text != sign else {return}
@@ -117,6 +126,7 @@ class ViewController: UIViewController {
         } else {
             screen.text = String(result)
         }
+        numberIsPositive = true
         
         
     }
